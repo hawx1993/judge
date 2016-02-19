f2099918af2280f0c5af8b7428867cca0f5c4547
trigkit4 <345823102@qq.com>
2016-02-19 17:25:15 +0800

    增加requirejs实例

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by trigkit4 on 16/2/19.
+ */
+requirejs.config({
+    paths: {
+        judge: 'judge'
+    }
+});
+requirejs(['judge'], function (judge) {
+    var val = [{'name':'huang'},123],
+        arr = [val,456];
+    console.log(judge.inArray(val,arr));//true
+});
